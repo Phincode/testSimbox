4a301e5edbc5749f56a30226d7195a5729d8f507
KOUADIO PHINEAS <32209043+Phincode@users.noreply.github.com>
2022-06-20 12:02:50 +0000

    bug correction

--- src/app/controllers/simbox.js
+++ src/app/controllers/simbox.js
@@ -1,27 +1,32 @@
 //module import
-const Port = require('serial-at');
 const SmsTransceiver = require('node-sms-transceiver');
+const SerialCommander = require('@westh/serial-commander') // or use import ... from ...
+
 
 exports.testSimBox=async(req,res)=>{
-    var response=await helloword();
+    var response=await main();
     res.json(response);
 }
 
-async function helloword(){
-    try {
-        // create serial connection
-    const port = new Port('COM28');
- 
-    // open serial connection
-    await port.open();
- 
-    // execute AT command and diaplay result
-    console.log(await port.at('AT'));
- 
-    // close serial connection
-    await port.close();
-    return "ok";
-    } catch (error) {
-        return error;
-    }
+
+const serialCommander = new SerialCommander({
+  port: 'COM28', // defaults to /dev/modem
+  //baudrate: 9600, // defaults to 115200
+  //readDelimiter = "\r", // defaults to '/n'
+ // writeDelimiter = "\n", // defaults to '/r/n'
+  disableLog: false, // defaults to false
+  defaultDelay: 50, // delay [ms] before the command is issued defaults to 100
+  log: string => console.log(`[${new Date().toISOString()}] ${string}`) // default logging function
+})
+
+async function main () {
+  const options = {
+    expectedResponses: ['OK', 'YEAH'], // defaults to ['OK']
+    timeout: 500,  // defaults to 1000
+    delay: 100 // defaults to defaultDelay set in the constructor
+  }
+  const response = await serialCommander.send('AT', options)
+  console.log(response)
+  serialCommander.close()
+  return response;
 }
